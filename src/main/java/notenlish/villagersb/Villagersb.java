ff7bc00c688299da6ce126995e1ef5928f074d77
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-04-05 12:13:08 +0300

    ability to transport villagers

--- src/main/java/notenlish/villagersb/Villagersb.java
+++ src/main/java/notenlish/villagersb/Villagersb.java
@@ -1,19 +1,35 @@
 package notenlish.villagersb;
 
+import com.mojang.brigadier.Message;
+import com.mojang.serialization.Codec;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentSyncPredicate;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntitySpawnReason;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.npc.villager.AbstractVillager;
 import net.minecraft.world.entity.npc.villager.Villager;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.TagValueOutput;
 import net.minecraft.world.level.storage.ValueOutput;
+import net.minecraft.world.phys.Vec3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +40,24 @@ public class Villagersb implements ModInitializer {
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static final AttachmentType<Boolean> PLAYER_HAS_BABY_VILLAGER_ATTACHMENT = AttachmentRegistry.create(
+            Identifier.fromNamespaceAndPath("villagers", "player_has_baby_villager_attachment"),
+            booleanBuilder -> booleanBuilder.initializer(() -> false)
+                    .syncWith(
+                            ByteBufCodecs.BOOL,
+                            AttachmentSyncPredicate.all()
+                    )
+    );
+    public static final AttachmentType<Boolean> PLAYER_HAS_VILLAGER_ATTACHMENT = AttachmentRegistry.create(
+            Identifier.fromNamespaceAndPath("villagersb","player_has_villager_attachment"),
+            booleanBuilder -> booleanBuilder.initializer(() -> false)
+                    .syncWith(
+                        ByteBufCodecs.BOOL,
+                        // sync data to all
+                        AttachmentSyncPredicate.all()
+                    )
+    );
 
     @Override
     public void onInitialize() {
@@ -38,23 +72,75 @@ public class Villagersb implements ModInitializer {
         DoStuff();
     }
 
+    private void SendMsgToChatEveryone(MinecraftServer server, Component msg) {
+        server.getPlayerList().broadcastSystemMessage(msg, false); // false is for chat
+    }
+
+    private void SpawnVillagerNearPlayer(MinecraftServer server, ServerPlayer player, boolean is_baby) {
+        var player_pos = player.position();
+        var look = player.getLookAngle();
+        var flatLook = new Vec3(look.x, 0, look.z);
+
+        SendMsgToChatEveryone(server, Component.literal("Player look angle " + look));
+
+        Villager villager = EntityType.VILLAGER.create(
+                server.getLevel(player.level().dimension()),
+                null,
+                new BlockPos(0,0,0),
+                EntitySpawnReason.NATURAL,
+                false,
+                false
+        );
+
+
+        if (villager != null) {
+            villager.setBaby(is_baby);
+            // move to infront of the player
+            villager.setPos(player_pos.add(flatLook.scale(1.5)).scale(1.0));
+            player.level().addFreshEntity(villager);
+        }
+    }
+
     private void DoStuff() {
         // register the payload client -> server
         PayloadTypeRegistry.serverboundPlay().register(ServerBoundVillagersbCarryPayload.TYPE, ServerBoundVillagersbCarryPayload.CODEC);
 
         ServerPlayNetworking.registerGlobalReceiver(ServerBoundVillagersbCarryPayload.TYPE, (payload, context) -> {
-                Entity entity = context.player().level().getEntity(payload.entityId());
+                Boolean player_has_villager = context.player().getAttachedOrSet(PLAYER_HAS_VILLAGER_ATTACHMENT, false);
+                Boolean player_has_baby = context.player().getAttachedOrSet(PLAYER_HAS_BABY_VILLAGER_ATTACHMENT, false);
 
-                if (entity instanceof Villager && entity instanceof LivingEntity ) {
-                    entity.save()
+                int ent_id = payload.entityId();
+                if (ent_id != -1) {
+                    Entity entity = context.player().level().getEntity(payload.entityId());
+                    if (entity instanceof Villager) {
+                        if (player_has_villager) {
+                            // stop carrying villager
+                            context.player().setAttached(PLAYER_HAS_VILLAGER_ATTACHMENT, false);
+                            LOGGER.info("Set attachment to false");
 
+                            SpawnVillagerNearPlayer(context.server(), context.player(), player_has_baby);
 
-                    entity.discard();  // remove without loot appearing
+                        } else {
+                            context.player().setAttached(PLAYER_HAS_VILLAGER_ATTACHMENT, true);
+                            LOGGER.info("Set attachment to true");
+                            context.player().setAttached(PLAYER_HAS_BABY_VILLAGER_ATTACHMENT, ((Villager) entity).isBaby());
+                            entity.discard();  // remove without loot appearing
+                        }
+                    }
+                } else {
+                    // just drop the villager if player is carrying it
+                    if (player_has_villager) {
+                        context.player().setAttached(PLAYER_HAS_VILLAGER_ATTACHMENT, false);
+                        LOGGER.info("Set attachment to false");
+
+                        SpawnVillagerNearPlayer(context.server(), context.player(), player_has_baby);
+                    }
                 }
+
 
                 // if (entity instanceof LivingEntity livingEntity && livingEntity.closerThan(context.player(), 5)) {
                 //    livingEntity.addEffect(new MobEffectInstance(MobEffects.GLOWING, 100));
                 // }
-        })
+        });
     }
 }
